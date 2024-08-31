16c306b00ff3c9b5489d72167d7ced31cad999d7
Gabriel Penteado <gabripenteado@gmail.com>
2024-08-31 16:40:31 -0300

    config prisma database client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma, PrismaClient, User } from '@prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject()
+  private readonly prisma: PrismaClient;
+
+  async createUser(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
+    return this.prisma.user.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({
+      where,
+    });
+  }
+}
